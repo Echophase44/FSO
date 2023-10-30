d8953102468975963a8de8c4237a4d08566e10b4
Nick <Echophase44@github.com>
2023-10-30 06:48:07 -0400

    Finished exercises 3.5-3.6

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -42,11 +44,54 @@ app.get("/info", (request, response) => {
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
+
   if (person) {
     response.json(person);
   } else {
     response.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.random() * 1000000000;
+};
+
+app.post("/api/persons/", (request, response) => {
+  const body = request.body;
+  let nameTaken = false;
+  persons.forEach((person) => {
+    if (person.name === body.name) {
+      nameTaken = true;
+    }
+  });
+
+  if (nameTaken) {
+    return response.status(400).json({
+      error: "This name is already taken.",
+    });
+  }
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "No content found.",
+    });
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
 });
 
 const PORT = 3001;
