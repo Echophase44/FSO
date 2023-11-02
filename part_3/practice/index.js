240b8621b7ec945cd70fa42f247d329bbff06967
Nick <Echophase44@github.com>
2023-11-02 16:20:37 -0400

    Complete app pushed to Fly.io

--- part_3/practice/index.js
+++ part_3/practice/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-
-app.use(express.json());
-app.use(requestLogger);
+const cors = require("cors");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -11,6 +9,10 @@ const requestLogger = (request, response, next) => {
   console.log("---");
   next();
 };
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(cors());
 
 let notes = [
   {
@@ -88,6 +90,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
