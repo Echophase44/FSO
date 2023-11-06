8fe0cac9e40f78d0f5696755dd24c91a9ae1b5bc
Nick <Echophase44@github.com>
2023-11-06 13:18:20 -0500

    Working on adding Mongodb to backend projects.

---
+++ part_2/workspace/workspace1/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://nfitzgerald745:${password}@cluster0.rtfx14x.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note({
+//   content: "HTML is Easy",
+//   important: true,
+// });
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// The object {} in here: .find({}) refers to the search conditions. Note.find({important: true}) is another search condition we can set.
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
