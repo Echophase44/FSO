ea2fd9c6d72242da3387636932c8e9181026f2cd
Nick <Echophase44@github.com>
2023-11-08 21:49:56 -0500

    updated practice app

--- part_2/workspace/workspace1/index.js
+++ part_2/workspace/workspace1/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+app.use(express.json());
 const cors = require("cors");
 const Note = require("./models/note");
 
@@ -12,7 +13,6 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
 app.use(express.static("dist"));
@@ -57,17 +57,37 @@ app.get("/api/notes", (request, response) => {
 //   }
 // });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end(); //if no note is found, this will trigger
+      }
+    })
+
+    // .catch((error) => {
+    //   console.log(error);
+    //   response.status(400).send({ error: "malformatted id" });
+    // });
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+// The old way
+// app.delete("/api/notes/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end();
+//   response.status(204).end();
+// });
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.param.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
@@ -113,11 +133,37 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, reqeust, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
+app.use(errorHandler); // has to be the last loaded middleware
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
